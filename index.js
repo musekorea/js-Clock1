a645eaf1059b6f1c85add48d8297b254fd2253c7
musekorea <musekorea@hanmail.net>
2021-03-27 06:19:36 +0800

    makePins

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const secondHand = document.querySelector('#secondHand');
 const minuteHand = document.querySelector('#minutedHand');
 const hourHand = document.querySelector('#hourHand');
+const pin = document.querySelectorAll('.pin');
+const container = document.querySelector('.container');
 
 setInterval(clock, 1000);
 function clock() {
@@ -22,3 +24,21 @@ function minuteMove(a) {
 function hourMove(a) {
   hourHand.style.transform = `rotate(${a * 30}deg)`;
 }
+
+for (let i = 0; i < 60; i++) {
+  const div = document.createElement('div');
+  div.classList.add('pinBox');
+  div.innerHTML = `
+  <div class="pin pinBlack">
+  <div class="pin pinWhite"></div>
+  </div>`;
+  container.appendChild(div);
+}
+let i = 0;
+const pinboxes = document.querySelectorAll('.pinBox');
+pinboxes.forEach((pinBox) => {
+  let degree = i * 6;
+  i++;
+  pinBox.style.transform = `rotate(${degree}deg) translateY(-50%)`;
+});
+/* pinbox.style.transform = `rotate(12deg)`; */
